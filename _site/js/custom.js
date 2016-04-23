99e9027c2b8ba5bcad2b54c119a88266b90942bf
franco bellomo <fnbellomo@hal>
2016-04-22 21:10:43 -0300

    correccion de la s in el titulo

---
+++ _site/js/custom.js
@@ -0,0 +1,9 @@
+/*****************************************
+ * Carousel options
+ ****************************************/
+$(function () {
+    $('#myCarousel').carousel({
+        interval:3000,
+        pause: "false"
+    });
+});
